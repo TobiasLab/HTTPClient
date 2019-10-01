fb5ff7526c3fdb7616cb3c877f24c67f735299b1
TobiasLab <tobias.labidi@gmail.com>
2019-10-01 17:34:02 +0200

    fixed pom file

---
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -0,0 +1,12 @@
+package no.kristiania.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void mathShouldWork() {
+        assertEquals(5, 2+2);
+    }
+}
